0ee96bf67ebb208171f5012ba1153280f1311823
hackira90 <gh.khoubaieb@gmail.com>
2019-10-26 13:20:35 +0200

    “HTML

--- warmUp9.js
+++ warmUp9.js
@@ -1,10 +1,34 @@
-// Create an HTML file
+
 // Create a CSS file
 // link the CSS file to the HTML file
-// The page should have a header, body and footer
+/ The page should have a header, body and footer
 // Change the background color of the body to be blue
 // Insert Three images next to each other
 // Create Two buttons and use different font family for each button
 // Create an unordered list with three anchor tags in it
 // Place In the footer a copyright claimer
 // Have Fun
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="styling" href="khoubaeib\my HTML\style\style.css">
+<meta charset="utf-8">
+<title >Styles Conference</title> 
+</head>
+<body>
+	<header>
+		<h1>
+			<a href="main.html">Styles Conference</a>
+<h1>Styles Conference</h1>
+<section>
+	<h1>Learning coding</h1>
+	<div>
+		<img src="https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj5hv63xLnlAhXO-qQKHYo-CtUQjRx6BAgBEAQ&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fcoding&psig=AOvVaw1M7a-p62ireNmhURrRhDGs&ust=1572165672813931" alt="coding">
+
+</div>
+
+</body>
+
+
+</html>
