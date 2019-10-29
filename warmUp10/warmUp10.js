50f0eb301708abf85831a85fe67695604e4cd155
hackira90 <gh.khoubaieb@gmail.com>
2019-10-29 10:15:05 +0100

    warmUp10

---
+++ warmUp10/warmUp10.js
@@ -0,0 +1,11 @@
+
+
+
+        var v1 = $("<li>Hello world</li>");
+       $('ul').append(li1);
+      var li2 = $("<li>i'm just</li>");
+       $('ul').append(li2);
+       var li3 = $("<li>coding</li>");
+       $('ul').append(li3);
+       var li4 = $("<li>coding</li>");
+       $('ul').append(li4);
