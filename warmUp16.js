9187210c83ae1e7d1fb51f4471656fa4441a2843
hackira90 <gh.khoubaieb@gmail.com>
2019-11-06 09:59:16 +0100

    warmUp16

--- warmUp16.js
+++ warmUp16.js
@@ -16,3 +16,32 @@
 // Constraints:
 // 1 <= text.length <= 10^4
 // text consists of lower case English letters only.
+
+
+function ballon(string) {
+	//if statement for minimum length
+	if(string.length <= 1){
+		return "please entre a word with at least 1 character";
+		//if statement for max length
+	} else if(string.length >= Math.pow(10,4) ){
+		return "please entre word with less character";
+	} 
+	var str = "";
+	var arr = string.split('');
+	var ball = 'balloon'.split('');
+	var word = 1;
+	var w = 0;
+//for loop to check letter from the string
+	for (var i = 0; i < arr.length; i++) {
+		//foor loopto check letter from the ballon word
+		for (var j = 0; j < ball.length; j++) {
+			//if statement to check matching letter
+			if(arr[i] === ball[j]){
+				arr.splice(i,1);
+				word++;
+			}
+		}
+	}
+	  w	= word/6;
+	return  "there is : "+ w + " " + "ballon word in " + string ;
+}
